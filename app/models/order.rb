cb2abad1b956814861fa51187f8b8b7bd3471741
chikara-dot <victorycastlepower0209@gmail.com>
2021-08-31 04:58:10 +0000

    バリデーション追加

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,3 +1,6 @@
 class Order < ApplicationRecord
   belongs_to :take
+  
+  validates :user_name,presence: true
+  validates :take_out,presence: true
 end
--- app/models/take.rb
+++ app/models/take.rb
@@ -1,3 +1,7 @@
 class Take < ApplicationRecord
   has_many :orders
+
+  validates :name,presence: true
+  validates :remaining,presence: true
+  validates :lowest,presence: true
 end
